a8a3fdea37ad1840db9de5f72388c153fadb5260
Beyza Mercan <beyzamercanse@gmail.com>
2023-09-20 22:25:04 +0300

    commit dv.py

--- dublicatevalue.py
+++ dublicatevalue.py
@@ -8,7 +8,7 @@ def has_duplicate_value(arr):
         steps += 1
         if num in existing_numbers:
             return True
-        existing_numbers[num] = 1
+        existing_numbers[num] = 1 #fix here
     
     print(steps)
     return False
@@ -16,16 +16,15 @@ result = has_duplicate_value([12,14,22,12])
 print(result) # true
 
 
-
 #
 #
 #
 #
 #
-
 
 
 # this is O(N^2) code
+
 def has_duplicate_value(arr):
     for i in range(len(arr)):
         for j in range(len(arr)):
